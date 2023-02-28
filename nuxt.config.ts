f94d1aff8a018639488e20e7d7f611e4d1f1092d
Arlen Molina <s3iyadragon@gmail.com>
2023-02-28 00:12:40 -0600

    0.0.1-fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,14 @@ export default {
         head: {
             charset: 'utf-16',
             viewport: 'width=device-width, initial-scale=1',
-            title: "",
+            title: "Arlen Molina -- Web developer",
             meta: [
                 {
                     name: 'description', 
                     content: `
-                        
+                        Find out whether a web developer is the right choice for you. 
+                        Discover how you can take you website to the next level and turn your vision into a reality!
+                        Get in touch today!
                     `
                 }
             ]
